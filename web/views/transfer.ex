cdfbe428064ebfd4ca80cf3b4f2bf6868e0b549a
Andrew Dryga <andrew@dryga.com>
2016-11-16 18:09:06 +0200

    Render external credential

--- web/views/transfer.ex
+++ web/views/transfer.ex
@@ -33,6 +33,13 @@ defmodule Tokenizer.Views.Transfer do
       number: hide_card_number(number)}
   end
 
+  def render("credential.json", %{credential: %{type: type, id: id, metadata: metadata}})
+      when type in ["external-credential"] do
+    %{type: type,
+      id: id,
+      metadata: metadata}
+  end
+
   defp hide_card_number(card_number) do
     String.slice(card_number, 1..6) <> String.duplicate("*", 6) <> String.slice(card_number, -4..-1)
   end

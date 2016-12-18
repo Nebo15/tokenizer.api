ee583d397d335b9610dcc491a938627d9c0ade17
Andrew Dryga <andrew@dryga.com>
2016-12-18 19:32:23 +0200

    Fixed #1: Require recipient phone for external credential transfers

--- lib/api/repo/schemas/peer.ex
+++ lib/api/repo/schemas/peer.ex
@@ -23,7 +23,22 @@ defmodule API.Repo.Schemas.Peer do
     |> validate_embed_type(:credential, ["card", "card-token"])
   end
 
-  def recipient_changeset(struct, params \\ %{}) do
+  def recipient_changeset(struct, params \\ %{})
+
+  # Recipient with external credential must have :phone
+  def recipient_changeset(struct, %{"credential" => %{"type" => "external-credential"}} = params) do
+    struct
+    |> apply_recipient_changeset(params)
+    |> validate_required([:phone])
+  end
+
+  # Other types
+  def recipient_changeset(struct, params) do
+    struct
+    |> apply_recipient_changeset(params)
+  end
+
+  defp apply_recipient_changeset(struct, params) do
     struct
     |> cast(params, [:phone, :email])
     |> cast_dynamic_embed(:credential)
